6b1729ccaf814264691a84c57cca7f6de0c2dfd5
Andres Flores <floresc.andres@gmail.com>
2018-04-22 18:17:42 +0000

    compare last letter, duplicate string x amount

--- Algorithms.js
+++ Algorithms.js
@@ -55,5 +55,28 @@ function largestOfFour(arr) {
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
+                    //-----Compare the last letter of a string to the taget-----
 
+function confirmEnding(str, target) {
+
+  return str.substr(-target.length) === target;
+
+}
+  
+confirmEnding("Bastian", "n");
+
+
+                              //-----duplicate the string x amount of time-----
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  
+  if (num < 0) {
+    return "";
+  } else {
+  return newStr = str.repeat(num);
+  }
+}
+
+repeatStringNumTimes("abc", 3);
 
