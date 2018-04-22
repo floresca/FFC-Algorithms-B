4f52a4ab1394abacdc78e151a0d927e4ff0454bb
Andres Flores <floresc.andres@gmail.com>
2018-04-22 04:41:37 +0000

    Solved finding max number of array within array as well as capitalize every letter in string

--- Algorithms.js
+++ Algorithms.js
@@ -1,4 +1,4 @@
-//-----Check for Palindromes-----
+                                              //-----Check for Palindromes-----
 
 function palindrome(str) {
   // Good luck!
@@ -12,8 +12,8 @@ function palindrome(str) {
 
 palindrome("A man, a plan, a canal. Panama");
 
-//-----Factorialize a numer -----
 
+                                              //-----Factorialize a numer -----
 
 function factorialize(num) {
   var newNum = 1;
@@ -24,5 +24,36 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+
+                         //-----Capitalize every first letter in a sentence-----
+
+function titleCase(str) {
+  var newStr = str.toLowerCase().split(" ");
+  
+  for(var i = 0 ; i < (newStr.length)  ; i++){
+    newStr[i] = newStr[i].charAt(0).toUpperCase() + newStr[i].substr(1);
+  }
+  return newStr.join(" ");
+} 
+
+titleCase("I'm a little tea pot");
+
+
+                              //-----Find largest numbers in several arrays-----
+                              
+function largestOfFour(arr) {
+  var newArr = [];
+  
+  for (var i = 0; i < arr.length; i++){
+      newArr.push(Math.max(...arr[i]));
+  }
+  
+  return newArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
 
 
